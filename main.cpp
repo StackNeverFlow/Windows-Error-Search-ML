1f0e8615fc74a353fbadb5566206477b9bc62fee
Linus <50374560+StackNeverFlow@users.noreply.github.com>
2020-07-01 19:27:06 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+   cout << "Searching for errors in windows.";
+   cout << "This may take a while";
+   sleep(3600);
+   cout << "No errors found."
+   return 0;
+}
